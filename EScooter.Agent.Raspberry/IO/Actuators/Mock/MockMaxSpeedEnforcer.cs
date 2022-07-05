faa3c1f35ac5c53f25a45e32bfbf42b4cd5d3f85
francescodente <dente.francesco1996@gmail.com>
2022-07-05 16:16:21 +0200

    Add mock max speed enforcer

---
+++ EScooter.Agent.Raspberry/IO/Actuators/Mock/MockMaxSpeedEnforcer.cs
@@ -0,0 +1,9 @@
+﻿using EScooter.Agent.Raspberry.Model;
+using UnitsNet;
+
+namespace EScooter.Agent.Raspberry.IO.Actuators.Mock;
+
+internal class MockMaxSpeedEnforcer : IActuator<Speed>
+{
+    public void SetValue(Speed value) => Console.WriteLine($"Setting Max Speed to {value.KilometersPerHour}km/h");
+}
