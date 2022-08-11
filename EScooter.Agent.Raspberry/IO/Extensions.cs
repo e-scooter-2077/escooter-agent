ff829f2d02b512f701a00412727229cd5b157670
francescodente <dente.francesco1996@gmail.com>
2022-08-11 15:43:22 +0200

    Setup DI for GPIO hardware

---
+++ EScooter.Agent.Raspberry/IO/Extensions.cs
@@ -0,0 +1,55 @@
+﻿using EScooter.Agent.Raspberry.IO.Actuators.Gpio;
+using EScooter.Agent.Raspberry.IO.Actuators.Mock;
+using EScooter.Agent.Raspberry.IO.Sensors.Gpio;
+using EScooter.Agent.Raspberry.IO.Sensors.Mock;
+using EScooter.Agent.Raspberry.Model;
+using Iot.Device.CharacterLcd;
+using System.Device.Gpio;
+
+namespace EScooter.Agent.Raspberry.IO;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMockHardware(this IServiceCollection services)
+    {
+        services.AddSingleton<ISpeedometer, MockSpeedometer>();
+        services.AddSingleton<IBatterySensor, MockBatterySensor>();
+        services.AddSingleton<IGpsSensor, MockGpsSensor>();
+
+        services.AddSingleton<IMagneticBrake, MockMagneticBrake>();
+        services.AddSingleton<IMaxSpeedEnforcer, MockMaxSpeedEnforcer>();
+        services.AddSingleton<ISpeedDisplay, MockSpeedDisplay>();
+        services.AddSingleton<IBatteryDisplay, MockBatteryDisplay>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddGpioHardware(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton(_ => new GpioController());
+
+        // TODO: when we have an ADC, use Gpio implementations instead of the Mock ones.
+        services.AddSingleton<ISpeedometer, MockSpeedometer>();
+        services.AddSingleton<IBatterySensor, MockBatterySensor>();
+        services.AddSingleton<IGpsSensor>(_ => new GpioGpsSensor(configuration.GetValue<string>("Gps:SerialPortName")));
+
+        services.AddSingleton<IMagneticBrake>(p => new GpioMagneticBrake(
+            configuration.GetValue<int>("Brakes:Pin"),
+            p.GetRequiredService<GpioController>()));
+
+        services.AddSingleton(p => LcdInterface.CreateGpio(
+            registerSelectPin: configuration.GetValue<int>("Lcd:RegisterSelectPin"),
+            enablePin: configuration.GetValue<int>("Lcd:EnablePin"),
+            dataPins: configuration.GetValue<int[]>("Lcd:DataPins"),
+            controller: p.GetRequiredService<GpioController>(),
+            shouldDispose: false));
+        services.AddSingleton<Lcd1602>();
+        services.AddSingleton<LcdDisplay>();
+
+        services.AddSingleton<IMaxSpeedEnforcer, GpioMaxSpeedEnforcer>();
+        services.AddSingleton<IBatteryDisplay, GpioBatteryDisplay>();
+        services.AddSingleton<ISpeedDisplay, GpioSpeedDisplay>();
+
+        return services;
+    }
+}
--- EScooter.Agent.Raspberry/Program.cs
+++ EScooter.Agent.Raspberry/Program.cs
@@ -1,6 +1,5 @@
 using EScooter.Agent.Raspberry;
-using EScooter.Agent.Raspberry.IO.Actuators.Mock;
-using EScooter.Agent.Raspberry.IO.Sensors.Mock;
+using EScooter.Agent.Raspberry.IO;
 using EScooter.Agent.Raspberry.IotHub;
 using EScooter.Agent.Raspberry.Model;
 
@@ -9,14 +8,14 @@ IHost host = Host.CreateDefaultBuilder(args)
     {
         services.AddHostedService<ScooterWorker>();
 
-        services.AddSingleton<ISpeedometer, MockSpeedometer>();
-        services.AddSingleton<IGpsSensor, MockGpsSensor>();
-        services.AddSingleton<IBatterySensor, MockBatterySensor>();
-
-        services.AddSingleton<IMagneticBrake, MockMagneticBrake>();
-        services.AddSingleton<IMaxSpeedEnforcer, MockMaxSpeedEnforcer>();
-        services.AddSingleton<ISpeedDisplay, MockSpeedDisplay>();
-        services.AddSingleton<IBatteryDisplay, MockBatteryDisplay>();
+        if (context.HostingEnvironment.IsDevelopment())
+        {
+            services.AddMockHardware();
+        }
+        else
+        {
+            services.AddGpioHardware(context.Configuration);
+        }
 
         services.AddSingleton<ScooterHardware>();
 
